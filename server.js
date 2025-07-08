037e97546db3b71e92766209704d84f0412bc7d0
MatiasJulianMartinez <matiasjulianmartinez2@gmail.com>
2025-07-08 16:22:51 -0300

    Commit Frontend Mundo Funko

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import cors from 'cors';
 
 dotenv.config();
+console.log("🚀 FRONTEND_URL cargado:", process.env.FRONTEND_URL); 
 connectDB();
 
 const app = express();
@@ -18,14 +19,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('<h1>Bienvenido/a a la API REST</h1>');
-});
-
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Bienvenido/a a la API REST</h1>');
+});
 
 routerAPI(app);
 
