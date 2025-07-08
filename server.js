6aba69052c81f2897f37e325bd9046433dfc376f
MatiasJulianMartinez <matiasjulianmartinez2@gmail.com>
2025-07-08 16:40:49 -0300

    Commit del backend

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: "https://mundofunko-final-frontend.onrender.com",
   credentials: true
 }));
 
