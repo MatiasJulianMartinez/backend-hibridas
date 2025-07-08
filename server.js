40ce29433c08c6cf21e5a8bfcd3e29d957868bd1
MatiasJulianMartinez <matiasjulianmartinez2@gmail.com>
2025-07-08 16:30:53 -0300

    Commit Frontend Mundo Funko

--- server.js
+++ server.js
@@ -5,37 +5,36 @@ import connectDB from './config/db.js';
 import cors from 'cors';
 
 dotenv.config();
-console.log("🚀 FRONTEND_URL cargado:", process.env.FRONTEND_URL);
+console.log("🚀 FRONTEND_URL cargado:", process.env.FRONTEND_URL); 
 connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// ✅ CORS configurado arriba de todas las rutas
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
-app.use(express.json());
-app.use(express.static("public"));
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>Bienvenido/a a la API REST</h1>');
+    res.status(200).send('<h1>Bienvenido/a a la API REST</h1>');
 });
 
 routerAPI(app);
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Algo salió mal' });
+    console.error(err.stack);
+    res.status(500).json({ error: 'Algo salió mal' });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
